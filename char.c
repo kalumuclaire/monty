3d9fe959748da0b73e971441378f412cb376a519
Makanda-h <hopemwaura39@gmail.com>
2023-11-18 12:39:39 -0800

    0x19

---
+++ char.c
@@ -0,0 +1,55 @@
+#include "monty.h"
+#include "lists.h"
+
+/**
+ * pchar_handler - this function does pchar handling
+ * @stack: this is the  double pointer to the stack
+ * @line_number: shows number of the line
+ */
+void pchar_handler(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node = *stack;
+
+	if (!node)
+	{
+		dprintf(STDERR_FILENO, PCHAR_FAIL, line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
+
+	if (node->n < 0 || node->n > 127)
+	{
+		dprintf(STDERR_FILENO, PCHAR_RANGE, line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
+
+	putchar(node->n);
+	putchar('\n');
+}
+
+/**
+ * pstr_handler - this function does  pstr instruction handling
+ * @stack: double pointer to the stack
+ * @line_number: shows number of the line
+ */
+void pstr_handler(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node = *stack;
+
+	(void)line_number;
+
+	if (!node)
+	{
+		putchar('\n');
+		return;
+	}
+
+	while (node && node->n != 0 && node->n >= 0 && node->n <= 127)
+	{
+		putchar(node->n);
+		node = node->next;
+	}
+
+	putchar('\n');
+}
---
+++ handler_funcs1.c
@@ -0,0 +1,111 @@
+#include "monty.h"
+#include "lists.h"
+
+/**
+ * pint_handler - this function handles pint command
+ * @stack: doubble pointer to stack
+ * @line_number: shows the number of the line
+ */
+void pint_handler(stack_t **stack, unsigned int line_number)
+{
+	stack_t *head = *stack;
+
+	if (!head)
+	{
+		dprintf(STDERR_FILENO, PINT_FAIL, line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", head->n);
+}
+
+/**
+ * pop_handler - this function handles pop command
+ * @stack: double pointer of stack
+ * @line_number: shows number of the line
+ */
+void pop_handler(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	if (!temp)
+	{
+		dprintf(STDERR_FILENO, POP_FAIL, line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
+
+	delete_dnodeint_at_index(stack, 0);
+}
+
+/**
+ * swap_handler - this function handles swap command
+ * @stack: double pointer of the staack
+ * @line_number: shows the number of the line
+ */
+void swap_handler(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack, *node = NULL;
+	int num;
+
+	if (dlistint_len(*stack) < 2)
+	{
+		dprintf(STDERR_FILENO, SWAP_FAIL, line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = get_dnodeint_at_index(*stack, 0);
+	num = temp->n;
+	delete_dnodeint_at_index(stack, 0);
+	node = insert_dnodeint_at_index(stack, 1, num);
+	if (!node)
+	{
+		dprintf(STDERR_FILENO, MALLOC_FAIL);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * add_handler - this function handles add command
+ * @stack: double pointer for the stack
+ * @line_number: shows the number of the line
+ */
+void add_handler(stack_t **stack, unsigned int line_number)
+{
+	int sum = 0;
+	stack_t *node = NULL;
+	stack_t *node_0 = get_dnodeint_at_index(*stack, 0);
+	stack_t *node_1 = get_dnodeint_at_index(*stack, 1);
+
+	if (dlistint_len(*stack) < 2)
+	{
+		dprintf(STDERR_FILENO, ADD_FAIL, line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
+
+	sum = node_0->n + node_1->n;
+	delete_dnodeint_at_index(stack, 0);
+	delete_dnodeint_at_index(stack, 0);
+	node = add_dnodeint(stack, sum);
+	if (!node)
+	{
+		dprintf(STDERR_FILENO, MALLOC_FAIL);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * nop_handler - this function handles nop command
+ * @stack: double pointer for the stack
+ * @line_number: shows the number of the line
+ */
+void nop_handler(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
